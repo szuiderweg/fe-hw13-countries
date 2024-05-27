7ed596015c25d8f6f4fc232682eb4c514297d4cb
szuiderweg <svmzuiderweg@hotmail.com>
2024-05-27 12:52:56 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import worldmap from './assets/world_map.png';
 
 function App() {
 
     return (
         <>
+            {/*dit is een wijziging*/}
             Maak je applicatie hier!
         </>
     )
