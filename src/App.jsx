3f198198ad0dee36dee25bfdc90a61d43636b943
szuiderweg <svmzuiderweg@hotmail.com>
2024-06-07 00:26:00 +0200

    print GET all countries to console, but API is too slow to continue with assigment

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,45 @@
 import './App.css';
-import worldmap from './assets/world_map.png';
+import worldMap from './assets/world_map.png';
+import {useState} from "react";
+import axios from "axios";
 
 function App() {
+    const [foundCountries, setFoundCountries] = useState([]);
+    async function getAllCountries(){
+        try{
+            const result = await axios.get('https://restcountries.com/v3.1/all?fields=name,population');
+            //! the restcountries API is so slow, does not respond to get all countries request, it times out.
+            // a query for a single country sometimes yields a response
+            //const result = await axios.get('https://api.chucknorris.io/jokes/random');
+            console.log(result);
+
+
+
+        } catch (e){
+            console.error(e);
+        }
+    }
+
+
+
 
     return (
         <>
-            {/*dit is een wijziging*/}
-            Maak je applicatie hier!
+            <header>
+                <img src={worldMap} alt={"world map"}/>
+                <h1>World Regions</h1>
+            </header>
+            <main>
+            {/*hier wordt de lijst zoekresultaten getoond    */}
+                <button
+                    type="button"
+                    onClick={() => getAllCountries()}
+                >Knop die iets doet</button>
+
+            {/*    vlag, naam , inwoners */}
+            </main>
+
+
         </>
     )
 }
