c85d84500018fb8f3e049d11cf5b56418ddb6f9c
szuiderweg <svmzuiderweg@hotmail.com>
2024-06-11 10:13:42 +0200

    GET request werkend gekregen met andere API

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,45 @@ import './App.css';
 import worldMap from './assets/world_map.png';
 import {useState} from "react";
 import axios from "axios";
+//todo:
+
+
 
 function App() {
+    //State dingen
     const [foundCountries, setFoundCountries] = useState([]);
+    const [ hidden, setHidden] = useState(true)
+
+    //handler functies
+    function handleAllCountries(){
+        void getAllCountries();//GET API call
+        console.log(foundCountries[0]);
+        // console.log(foundCountries);
+
+    }
+
+
+    //API requests
     async function getAllCountries(){
         try{
-            const result = await axios.get('https://restcountries.com/v3.1/all?fields=name,population');
+            //const result = await axios.get('https://restcountries.com/v3.1/all?fields=name,population');
             //! the restcountries API is so slow, does not respond to get all countries request, it times out.
-            // a query for a single country sometimes yields a response
+            // a query for a single country sometimes yields a response. Chuck Norris API from lesson 13 example does work:
             //const result = await axios.get('https://api.chucknorris.io/jokes/random');
-            console.log(result);
+            //possible solution is to use 2 other API's:  https://countryinfoapi.com/ for information about countries
+            //https://flagsapi.com/ for the flag images
 
+            const result = await axios.get('https://countryinfoapi.com/api/countries');
 
+            //console.log(result.data[0].name);
+            // console.log(result.data);
+            setFoundCountries(result.data);//update State with received data
 
         } catch (e){
             console.error(e);
         }
     }
+
 
 
 
@@ -33,9 +55,16 @@ function App() {
             {/*hier wordt de lijst zoekresultaten getoond    */}
                 <button
                     type="button"
-                    onClick={() => getAllCountries()}
-                >Knop die iets doet</button>
+                    onClick={() => handleAllCountries()}
+                >Alle landen</button>
+                { foundCountries !== [] && <section>
+                    <article className="country-card">
+                        <p> ik ben er wel!</p>
+                        {/*<h4>{foundCountries[0].name}</h4>*/}
+                        {/*<p>Has a population of {foundCountries[0].population} people</p>*/}
+                    </article>
 
+                </section> }
             {/*    vlag, naam , inwoners */}
             </main>
 
